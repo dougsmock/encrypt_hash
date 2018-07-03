1447ce83f182ae833b18b10f38ccf12da568bddf
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-07-03 16:25:57 -0400

    attempting Jeremy's example, first fail test.

--- encrypt_hash.rb
+++ encrypt_hash.rb
@@ -4,13 +4,21 @@ def encrypt(alphabet)
   letters_right_end = ["a".."e"]
   moved = letters_shifted + letters_right_end
 
-  index = 0
-  while index < 26
-    {letters[index] => moved[index]}
-    index += 1
-  end
-end
+  arr = "me".slice("")
 
+  enc_arr = []
+
+  arr.each do |letter|
+    counter = 0
+    letters.each do |let|
+      counter += 1
+      if let == letter
+        enc_arr << counter
+      end
+    end
+  end
+    enc_arr
+end
 
 def decode(moved)
   if moved[21] != nil
